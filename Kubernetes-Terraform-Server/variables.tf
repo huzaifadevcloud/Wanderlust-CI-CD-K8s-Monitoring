018cf991d3eed52e806c71705c2646898a60dbf9
Huzaifa Hatim <huzaifahatim2000@hotmail.com>
2025-08-24 04:42:42 +0500

    Update variables.tf

--- Kubernetes-Terraform-Server/variables.tf
+++ Kubernetes-Terraform-Server/variables.tf
@@ -6,3 +6,6 @@ variable "azs" {}
 variable "public_subnet_cidrs" {}
 variable "private_subnet_cidrs" {}
 
+#right now your variables.tf just defines the variable names without values, so Terraform will:
+#Ask you for values at runtime when you run terraform apply, or
+#Expect you to provide values in a terraform.tfvars file, or via -var flags in CLI.
